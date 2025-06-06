f1d5d0af43086b5a19681438486327800e3057e8
Vysakh P <vysakhp17@gmail.com>
2025-06-06 22:28:01 +0530

    feat: integrate Firebase into Angular application
    - Added @angular/fire package to package.json for Firebase support.
    - Updated app.config.ts to initialize Firebase app and provide authentication and Firestore services.
    - Created firebase.json for Firebase configuration.
    - Added environment files for development and production with Firebase configuration details.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        projectId: "my-recipe-book-b9c48",
+        appId: "1:1028452697046:web:6c5f9a850228fa07da9a77",
+        storageBucket: "my-recipe-book-b9c48.firebasestorage.app",
+        apiKey: "AIzaSyCn-94hadNJ3sZBw6Pnjb6SMvI3Gr2P-CM",
+        authDomain: "my-recipe-book-b9c48.firebaseapp.com",
+        messagingSenderId: "1028452697046"
+    },
+};
