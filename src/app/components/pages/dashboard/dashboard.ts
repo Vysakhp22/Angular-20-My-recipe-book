18233c2815907f498cf73a78631b4c840eee91f3
Vysakh P <vysakhp17@gmail.com>
2025-06-29 13:56:59 +0530

    feat: add dashboard widget component to enhance dashboard functionality

--- src/app/components/pages/dashboard/dashboard.ts
+++ src/app/components/pages/dashboard/dashboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UserRegister } from "../user-register/user-register";
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { DashbroardWidget } from "../dashbroard-widget/dashbroard-widget";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterLinkActive, RouterLink],
+  imports: [RouterLinkActive, RouterLink, DashbroardWidget],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
