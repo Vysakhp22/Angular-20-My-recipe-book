22073c4637004371bd080313cd9e72b3ee7b82dd
Vysakh P <vysakhp17@gmail.com>
2025-06-29 22:19:04 +0530

    feat: add recent recipes component to dashboard with layout and styling

---
+++ src/app/components/pages/dashboard-recent-recipes/dashboard-recent-recipes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-recent-recipes',
+  imports: [],
+  templateUrl: './dashboard-recent-recipes.html',
+  styles: ``
+})
+export class DashboardRecentRecipes {
+
+}
--- src/app/components/pages/dashboard-widget/dashboard-widget.ts
+++ src/app/components/pages/dashboard-widget/dashboard-widget.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Countup } from '@core/directives/countup';
+import { DashboardRecentRecipes } from "../dashboard-recent-recipes/dashboard-recent-recipes";
 
 @Component({
   selector: 'app-dashboard-widget',
-  imports: [Countup],
+  imports: [Countup, DashboardRecentRecipes],
   templateUrl: './dashboard-widget.html',
   styles: ``
 })
