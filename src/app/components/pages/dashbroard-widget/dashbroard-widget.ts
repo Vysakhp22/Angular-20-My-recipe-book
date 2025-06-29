30949381bb318aaef92b987d76e1670a69adc5b0
Vysakh P <vysakhp17@gmail.com>
2025-06-29 13:54:21 +0530

    feat: update dashboard layout with avatar dropdown and enhance component styling

---
+++ src/app/components/pages/dashbroard-widget/dashbroard-widget.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashbroard-widget',
+  imports: [],
+  templateUrl: './dashbroard-widget.html',
+  styles: ``
+})
+export class DashbroardWidget {
+
+}
